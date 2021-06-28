1766ba34d576212f7dd527f5f25efd86cc52c757
guilhermealcar <guiialcar@hotmail.com>
2021-06-28 11:13:12 -0300

    Revisao

--- ex02.c
+++ ex02.c
@@ -4,23 +4,39 @@ caractere ‘*’. Além disso, mostre na tela quantas linhas esse arquivo possu
 o controle de erros, isto é, insira comandos que mostre se os arquivos foram abertos com sucesso, e caso
 contrário, imprima uma mensagem de erro encerrando o programa*/
 
+#include <stdlib.h>
 #include <stdio.h>
 
-int main(){
-    FILE *arq1, *arq2;
-    arq1 = fopen("old.txt", 'r');
-    arq2 = fopen("new.txt", 'w');
+
+int main() {
+    FILE *arq, *arq2;
+    arq = fopen("ex02old.txt","r");
+    arq2 = fopen("ex02new.txt","w");
 
     // Se houver erro na abertura do arquivo
-    if(arq1 == NULL || arq2 == NULL){ 
-        printf("Erro ao abrir o arquivo!\n");
+    if(arq == NULL || arq2 == NULL ){
+        printf("Erro ao abrir o arquivo!");
         exit(1);
     }
-
-    int linhas=0;
+    
+    int linhas = 1;
     char letra;
 
-    
+    while((letra = fgetc(arq)) != EOF ){
+        if(letra == 'A' || letra == 'a' || letra == 'E' || letra == 'e' || letra == 'I' || letra == 'i' || letra == 'O' || letra == 'o' || letra == 'U' || letra == 'u'){
+            letra = '*';
+        }
+        fprintf(arq2, "%c", letra);
+
+        if(letra == '\n'){
+            linhas++;
+        }
+    }
+
+    printf("O arquivo possui %d linhas.\n", linhas);
+
+    fclose(arq);
+    fclose(arq2);
 
     return 0;
 }
